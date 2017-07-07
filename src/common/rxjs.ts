6477a264d5359f0af6789108b0a83de7ef10ed1f
Oden <ops991923@gmail.com>
2017-07-07 16:37:52 -0700

    Add `rxjs.ts`, which adds all needed operators.

---
+++ src/common/rxjs.ts
@@ -0,0 +1,7 @@
+import "rxjs/add/observable/bindCallback";
+import "rxjs/add/observable/dom/ajax";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/switchMap";
+import "rxjs/add/operator/toPromise";
+
+export { Observable } from "rxjs/Observable";
