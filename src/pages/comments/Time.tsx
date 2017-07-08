1362d71c701995be158bc5e2576e64da52fe95e4
Oden <ops991923@gmail.com>
2017-07-07 19:26:30 -0700

    Make Time update every 10 seconds.

--- src/pages/comments/Time.tsx
+++ src/pages/comments/Time.tsx
@@ -3,15 +3,30 @@ import { translate } from "react-i18next";
 
 import { format } from "common/time";
 
-export const Time = translate("time")((props: TimeProps) => {
-	const { created } = props;
-	const age = (Date.now() / 1000) - created;
-	const date = new Date(created * 1000);
+const UPDATE_INTERVAL = 10000;
 
-	return (
-		<time title={date.toLocaleString()} dateTime={date.toISOString()}>{format(age)}</time>
-	);
-});
+@translate("time")
+export class Time extends React.Component<TimeProps, {}> {
+	private intervalId: any;
+
+	componentDidMount() {
+		this.intervalId = setInterval(this.forceUpdate.bind(this), UPDATE_INTERVAL);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalId);
+	}
+
+	render() {
+		const { created } = this.props;
+		const age = (Date.now() / 1000) - created;
+		const date = new Date(created * 1000);
+
+		return (
+			<time title={date.toLocaleString()} dateTime={date.toISOString()}>{format(age)}</time>
+		);
+	}
+}
 
 interface TimeProps {
 	created: number;
