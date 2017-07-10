5c8b315c23660314eaaa0f88b8be78a24b0aa91a
Oden <ops991923@gmail.com>
2017-07-09 18:10:45 -0700

    Change Footer to use ActionList, and consolidate reply logic into it.

--- src/pages/comments/post/Footer.tsx
+++ src/pages/comments/post/Footer.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
-import classnames from "classnames";
 
 import { save } from "common/reddit-api";
 
-import style from "./Footer.scss";
+import { ActionList } from "../ActionList";
+import { Reply } from "./Reply";
 
 @translate("footer")
 export class Footer extends React.Component<FooterProps, FooterState> {
 	state: FooterState = {
+		replyOpen: false,
 		saved: false
 	};
 
@@ -19,47 +20,60 @@ export class Footer extends React.Component<FooterProps, FooterState> {
 		this.setState({ saved: !this.state.saved });
 	}
 
+	toggleReply = () => {
+		this.setState({ replyOpen: !this.state.replyOpen });
+	}
+
 	componentDidMount() {
 		this.setState({ saved: this.props.saved });
 	}
 
 	render() {
 		const t = this.props.t!;
-		const { children, className, modhash, permalink } = this.props;
+		const { className, id, linkId, modhash, permalink } = this.props;
 
 		return (
-			<ul className={classnames(style.footer, className)}>
-				<li>
+			<div className={className}>
+				<ActionList>
 					<a target="_blank" href={permalink}>
 						{t("permalink")}
 					</a>
-				</li>
 
-				{modhash && (
-					<li>
+					{modhash && (
 						<button onClick={this.onClickSave}>
 							{t(this.state.saved ? "unsave" : "save")}
 						</button>
-					</li>
-				)}
+					)}
 
-				{React.Children.map(children, child => (
-					<li>
-						{child}
-					</li>
-				))}
-			</ul>
+					{modhash && (
+						<button onClick={this.toggleReply}>
+							{t("reply")}
+						</button>
+					)}
+				</ActionList>
+
+				{this.state.replyOpen ? (
+					<Reply
+						modhash={modhash}
+						onClose={this.toggleReply}
+						linkId={linkId}
+						parentId={id}
+					/>
+				) : null}
+			</div>
 		);
 	}
 }
 
 export interface FooterProps extends InjectedTranslateProps, React.HTMLProps<HTMLUListElement> {
 	id: string;
+	linkId: string;
 	modhash: string;
 	permalink: string;
 	saved: boolean;
 }
 
 interface FooterState {
+	replyOpen: boolean;
 	saved: boolean;
 }
