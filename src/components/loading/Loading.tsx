5cd041b79c6fd8e5c76d0d89f105e5f7cda52b2f
Oden <ops991923@gmail.com>
2017-07-10 15:30:40 -0700

    Add spinning Loading component.

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classnames from "classnames";
+
+import loading from "assets/loading.svg";
+import style from "./Loading.scss";
+
+export const Loading = ({ className }: React.HTMLProps<HTMLImageElement>) => (
+	<div className={classnames(style.loading, className)}>
+		<img src={loading} />
+	</div>
+);
