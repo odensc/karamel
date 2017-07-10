0474a058073101f76c2a0246a76fd47b331db7f7
Oden <ops991923@gmail.com>
2017-07-09 18:09:50 -0700

    Add amount of comments to PostItem title.

--- src/pages/comments/post-list/PostItem.tsx
+++ src/pages/comments/post-list/PostItem.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import { Post } from "data/reddit";
 
 export const PostItem = ({ onClick, post, ...props }: PostListProps) => (
-	<button {...props} onClick={onClick}>{post.subreddit}</button>
+	<button {...props} onClick={onClick}>
+		{post.subreddit} <span style={{color: "#888"}}>({post.num_comments})</span>
+	</button>
 );
 
 export interface PostListProps extends React.HTMLProps<HTMLButtonElement> {
