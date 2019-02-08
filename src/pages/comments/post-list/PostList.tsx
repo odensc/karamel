161122a0daa873164fdb0b4703cb5de8cc52a39f
Oden <hello@odensc.me>
2019-02-08 12:33:50 -0800

    Add official sub marker

--- src/pages/comments/post-list/PostList.tsx
+++ src/pages/comments/post-list/PostList.tsx
@@ -8,25 +8,37 @@ import style from "./PostList.scss";
 
 export const PostList = ({
 	activePostId,
+	description,
 	onPostClick,
 	posts
-}: PostListProps) => (
-	<div className={style.list}>
-		{posts.map(post => (
-			<PostItem
-				key={post.name}
-				className={classnames(style.item, {
-					[style.active]: post.name === activePostId
-				})}
-				onClick={onPostClick.bind(null, post)}
-				post={post}
-			/>
-		))}
-	</div>
-);
+}: PostListProps) => {
+	const sub = (description.match(/reddit\.com\/r\/([^ /\n]+)/) || [])[1];
+
+	return (
+		<div className={style.list}>
+			{posts
+				.sort(post => (post.subreddit === sub ? -1 : 0))
+				.map(post => (
+					<PostItem
+						key={post.name}
+						className={classnames(style.item, {
+							[style.active]: post.name === activePostId,
+							[style.official]: post.subreddit === sub
+						})}
+						onClick={onPostClick.bind(null, post)}
+						post={post}
+						title={
+							post.subreddit === sub ? "Official Subreddit" : ""
+						}
+					/>
+				))}
+		</div>
+	);
+};
 
 export interface PostListProps {
 	activePostId: string;
+	description: string;
 	onPostClick: (post: Post) => void;
 	posts: Post[];
 }
