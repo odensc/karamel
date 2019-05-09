058ac2eb5aeaab329424af04adc6ca4675176418
Oden <hello@odensc.me>
2019-05-08 18:11:18 -0700

    Remove crx packing

--- package.sh
+++ package.sh
@@ -5,6 +5,4 @@ cd dist
 zip -r ../builds/$VERSION/out.zip ./*
 cd ../builds/$VERSION
 mv out.zip $VERSION.zip
-chromium --disable-gpu --pack-extension=$(readlink -f ../../dist) --pack-extension-key=$(readlink -f ../../key.pem)
-mv ../../dist.crx $VERSION.crx
 cd ../..
