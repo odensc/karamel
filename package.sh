6045f5a154ea813a14de91045e0b4dcda355bb16
Oden <hello@odensc.me>
2019-06-03 12:42:05 -0700

    Misc updates

--- package.sh
+++ package.sh
@@ -2,7 +2,4 @@ VERSION=$(jq -r ".version" src/assets/static/manifest.json)
 
 mkdir -p builds/$VERSION
 cd dist
-zip -r ../builds/$VERSION/out.zip ./*
-cd ../builds/$VERSION
-mv out.zip $VERSION.zip
-cd ../..
+zip -r ../builds/$VERSION/$VERSION.zip ./*
